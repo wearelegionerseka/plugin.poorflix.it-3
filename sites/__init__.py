7174eaad66623c8c72cab981c68f05d36bb5cf1e
An0nimia <41164945+An0nimia@users.noreply.github.com>
2021-03-09 22:39:27 +0100

    POORFLIX FOR KODI MATRIX

---
+++ sites/__init__.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+
+from requests import get
+from importlib import import_module
+
+where = "https://raw.githubusercontent.com/An0nimia/plugin.poorflix.it/master/sites.json"
+sites = get(where).json()
+sites_film = []
+sites_serietv = []
+
+films = [
+	"altadefinizione7", "altadefinizione1", "ilgeniodellostreaming2",
+	"altadefinizione3", "altadefinizione5", "ilgeniodellostreaming1",
+	"altadefinizione4", "altadefinizione2", "altadefinizione6",
+	"piratestreaming", "cineblog01"
+]
+
+serietvs = [
+	"eurostreaming1", "eurostreaming2", "eurostreaming3",
+	"piratestreaming", "italiaserie", "serietvu"
+]
+
+for a in films:
+	library = import_module("sites.%s" % a)
+	c = sites['sites'][a]
+	works = c['works']
+
+	if not works:
+		continue
+
+	library.host = c['link']
+	library.timeout = c['timeout']
+	library.is_cloudflare = c['is_cloudflare']
+	sites_film.append(library)
+
+for a in serietvs:
+	library = import_module("sites.%s" % a)
+	c = sites['sites'][a]
+	works = c['works']
+
+	if not works:
+		continue
+
+	library.host = c['link']
+	library.timeout = c['timeout']
+	library.is_cloudflare = c['is_cloudflare']
+	sites_serietv.append(library)
